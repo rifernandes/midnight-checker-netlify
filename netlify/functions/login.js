f0e3a149db88a085b54900c421647596ce5c944d
rifernandes <rian30376@gmail.com>
2025-04-23 20:03:17 -0300

    Versão inicial - Netlify

---
+++ netlify/functions/login.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+// Defina no Netlify UI como variável de ambiente
+// e
+const SECRET = process.env.JWT_SECRET;
+const USER = 'novousuario';
+const PASS = 'cliente32.li.com';
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+  const { username, password } = JSON.parse(event.body);
+  if (username === USER && password === PASS) {
+    const token = jwt.sign({ sub: USER }, SECRET, { expiresIn: '4h' });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ token }),
+    };
+  }
+  return { statusCode: 401, body: 'Credenciais inválidas' };
+};
